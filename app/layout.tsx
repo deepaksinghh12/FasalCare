657bd9889381b39d3fa0add543e8086d1b102c09
Deepak Singh <110714950+deepaksinghh12@users.noreply.github.com>
2025-10-01 14:17:17 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'AgriMitra',
-  description: 'Made with ❤️ by Akshay',
+  description: 'Made with ❤️',
 }
 
 export default function RootLayout({
